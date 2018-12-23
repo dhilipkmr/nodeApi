8012804c3fec5de3f2980bbc42a08a661ee09096
Dhilip kumar Velusamy <dhilip1211@gmail.com>
2018-12-23 12:16:25 +0530

    Update config.js

--- server/config/config.js
+++ server/config/config.js
@@ -6,4 +6,6 @@ if (env === 'development') {
 } else if (env === 'test') {
   process.env.PORT = 3000;
   process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoAppTest';
+} else {
+  process.env.MONGODB_URI = 'mongodb://dhilipk13:dhilipk13@ds247310.mlab.com:47310/expense';
 }
