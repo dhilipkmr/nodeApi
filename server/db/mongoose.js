5bc12ed9fad1f00b009a7ea887ca8effe08736d6
Dhilip kumar Velusamy <dhilip1211@gmail.com>
2018-12-23 12:12:39 +0530

    Update mongoose.js

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGODB_URI, {useMongoClient : true});
+mongoose.connection.openUri(process.env.MONGODB_URI);
 
 module.exports = {mongoose};
